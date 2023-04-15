e8711dadd162a0f45c42124aac25e175769fe902
Bilal-hajM <bblal1003@gmail.com>
2023-04-15 04:55:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(name, eventName) {
+  const messages = [];
+
+  for (let i = 0; i < name.length; i++) {
+   const message = `Thank you, ${name[i]}, for the wonderful ${eventName} gift!`
+   messages.push(message)
+  }
+return messages;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+} 
